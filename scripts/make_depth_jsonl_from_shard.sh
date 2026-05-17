d607e4a2189f2b9a57cedf1090f8192fdc5110ed
minhdov <dovanminh1000@gmail.com>
2026-05-17 23:17:33 +0800

    model 1.1 to 1.3

--- scripts/make_depth_jsonl_from_shard.sh
+++ scripts/make_depth_jsonl_from_shard.sh
@@ -1,5 +1,5 @@
 python make_libero_depth_jsonl_shards.py \
-  --depth_root /datasets/libero_ssv2/frames_train \
-  --out_dir /datasets/libero_ssv2/shards_rgb \
+  --depth_root /datasets/ssv2_libero90/frames_train \
+  --out_dir /datasets/ssv2_libero90/shards_rgb \
   --num_shards 4 \
   --prefix frames_train
--- scripts/split_folders_to_shards.sh
+++ scripts/split_folders_to_shards.sh
@@ -1,5 +1,5 @@
 python split_folders_to_shards.py \
-  --root /datasets/libero_ssv2/frames_train \
-  --out_dir /datasets/libero_ssv2/shards_rgb \
+  --root /datasets/ssv2_libero90/frames_train \
+  --out_dir /datasets/ssv2_libero90/shards_rgb \
   --num_shards 4 \
   --prefix train_folders
