ca8e1a49c5fb28a453d98a0e3ee90fe9a0e6111a
minhdov <dovanminh1000@gmail.com>
2026-05-18 15:01:32 +0800

    add model 1.5

--- scripts/make_depth_jsonl_from_shard.sh
+++ scripts/make_depth_jsonl_from_shard.sh
@@ -1,5 +1,11 @@
-python make_libero_depth_jsonl_shards.py \
-  --depth_root /datasets/ssv2_libero90/frames_train \
-  --out_dir /datasets/ssv2_libero90/shards_rgb \
+# python make_libero_depth_jsonl_shards.py \
+#   --depth_root /datasets/ssv2_libero90/frames_train \
+#   --out_dir /datasets/ssv2_libero90/shards_rgb \
+#   --num_shards 4 \
+#   --prefix frames_train
+
+  python make_libero_depth_jsonl_shards.py \
+  --depth_root /datasets/ssv2_libero90/depth_train \
+  --out_dir /datasets/ssv2_libero90/shards_depth \
   --num_shards 4 \
-  --prefix frames_train
+  --prefix depth_train
